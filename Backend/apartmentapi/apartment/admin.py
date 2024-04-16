3de112addd016770e5e9a5a377df83f06860f527
Msang2003 <msang.nms@gmail.com>
2024-04-16 21:55:24 +0700

    upodate

---
+++ Backend/apartmentapi/apartment/admin.py
@@ -0,0 +1,63 @@
+import cloudinary
+from django.contrib import admin
+from django.db.models import Count
+from django.template.response import TemplateResponse
+from django.utils.html import mark_safe
+from apartment.models import Category, Course, Lesson, User, Tag, Comment, Like
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django.urls import path
+
+
+class MyCourseAdminSite(admin.AdminSite):
+    site_header = 'eCourseOnline'
+
+    def get_urls(self):
+        return [path('course-stats/', self.stats_view)] + super().get_urls()
+
+    def stats_view(self, request):
+        course_stats = Category.objects.annotate(c=Count('course__id')).values('id', 'name', 'c')
+        return TemplateResponse(request, 'admin/stats.html', {
+            "course_stats": course_stats
+        })
+
+
+admin_site = MyCourseAdminSite(name='iCourse')
+
+
+class CourseForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorUploadingWidget)
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class MyCourseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_date', 'updated_date', 'active']
+    search_fields = ['name', 'description']
+    list_filter = ['id', 'created_date', 'name']
+    readonly_fields = ['my_image']
+    form = CourseForm
+
+
+    def my_image(self, instance):
+        if instance:
+            if instance.image is cloudinary.CloudinaryResource:
+                return mark_safe(f"<img width='120' src='{instance.image.url}' />")
+
+            return mark_safe(f"<img width='120' src='/static/{instance.image.name}' />")
+
+    class Media:
+        css = {
+            'all': ('/static/css/style.css', )
+        }
+
+
+admin_site.register(Category)
+admin_site.register(Course, MyCourseAdmin)
+admin_site.register(Lesson)
+admin_site.register(User)
+admin_site.register(Tag)
+admin_site.register(Comment)
+admin_site.register(Like)
---
+++ Backend/apartmentapi/apartment/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from apartment import views
+
+r = routers.DefaultRouter()
+r.register('categories', views.CategoryViewSet, basename='categories')
+r.register('apartment', views.CourseViewSet, basename='apartment')
+r.register('lessons', views.LessonViewSet, basename='lessons')
+r.register('users', views.UserViewSet, basename='users')
+r.register('comments', views.CommentViewSet, basename='comments')
+
+urlpatterns = [
+    path('', include(r.urls))
+]
